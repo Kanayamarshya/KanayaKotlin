1f6cd65ceb95eb106f227d84ff9b444189cf267d
Kanaya Marshya Saleha <150243172+Kanayamarshya@users.noreply.github.com>
2024-01-27 11:44:29 +0700

    Update belanja.kt

--- belanja.kt
+++ belanja.kt
@@ -1,34 +1,35 @@
 //fungsi dengan return
-fun barang(item : String):Int{
-    return when (item){
-    	"Gamis" -> 150000
+fun barang(item: String): Int {
+    return when (item) {
+        "Gamis" -> 150000
         "Kaos" -> 50000
         "Kemeja" -> 700000
         "Celana" -> 120000
         "Kaos Dalam" -> 20000
-        
         else -> 0
-    } 
+    }
 }
 
-fun diskon(harga: Int, diskon: Double): Int{
+fun diskon(harga: Int, diskon: Double): Int {
     return (harga * diskon).toInt()
-    
 }
-fun total(harga: Int, hargaDiskon: Int): Int{
-    return (harga - hargaDiskon).toInt()
-    
+
+fun total(harga: Int, hargaDiskon: Int): Int {
+    return harga - hargaDiskon
 }
 
 fun main() {
     val namaBarang = "Gamis"
-    val diskon = 0.05
+    val diskon1 = 0.1
+    val diskon2 = 0.05
     val harga = barang(namaBarang)
-    val hargaDiskon= if (harga>100000) diskon(harga,diskon) else 0
+    val hargaDiskon = when {
+        harga > 100000 -> diskon(harga, diskon1)
+        harga > 80000 -> diskon(harga, diskon2)
+        else -> 0
+    }
     val hargaTotal = total(harga, hargaDiskon)
     println("Baju yang anda beli: \nNama barang\t: $namaBarang \nHarga Barang\t: Rp. $harga")
     println("Diskon \t\t: Rp. $hargaDiskon")
     println("Total Bayar \t: Rp. $hargaTotal")
-    
 }
-
